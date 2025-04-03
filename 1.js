1b788c787639f4a0dfc42efe527c10c33474282b
vamp-ctrl <victoraugustomauriciopereira@gmail.com>
2025-04-03 11:24:43 -0400

    Questões

---
+++ 1.js
@@ -0,0 +1,19 @@
+// questão 1
+let nome = "Victor";
+let idade = 15;
+console.log(`Olá, meu nome é ${nome} e eu tenho ${idade} anos.`)
+
+// questão 2
+let escola = "ifro";
+console.log(escola.toLowerCase());
+console.log(escola.toUpperCase());
+
+// questão 3
+let id = 69;
+console.log(typeof id)
+
+//questão 4
+let peso = 60;
+let altura = 1.70;
+let soma1 = peso / (altura * altura)
+console.log(`IMC = ${soma1}`)
