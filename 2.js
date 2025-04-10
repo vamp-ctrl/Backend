911e35a9b496da8cbf502d4a88736fa319f5a0c7
vamp-ctrl <victoraugustomauriciopereira@gmail.com>
2025-04-10 11:27:04 -0400

    exercicios 2

--- 2.js
+++ 2.js
@@ -10,16 +10,14 @@ console.log(`${num1 } / ${num2} = ${num1 / num2}`)
 //quetão 2
 let pessoa1 = 18;
 let pessoa2 = 19;
+let resultado = pessoa1 > pessoa2 ? "Primeira pessoa mais velha" : "Seguna pessoa pessoa mais velha"
 
 //questão 3
 let idade = 17;
-let verificação = idade<17;
-console.log(``)
+let verificação = idade <= 18 ? "Pessoa menor de idade" : "Pessoa maior de idade"
+console.log(verificação)
 
 //questão 4
 let num = 3;
-let resto = num / 2;
-let resto1 = num /2;
-if (resto  % 0) {
-console.log("Númerp ímar")
-}
+let verificar = num % 2 != 0 ? "O número é impar" : "O número é par"
+console.log(verificar)
