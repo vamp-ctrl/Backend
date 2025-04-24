375dd3a078cfc301cedfb8228bc8217291908982
vamp-ctrl <victoraugustomauriciopereira@gmail.com>
2025-04-24 11:38:29 -0400

    dfgsdfd

--- Objetos/1.js
+++ Objetos/1.js
@@ -6,7 +6,7 @@ let apartamento = {
     andar: 7
     };
     
-    console.log(`Quetão 1\n Apartamento com ${apartamento.quartos} quartos, localizado no ${apartamento.andar}º andar da ${apartamento.endereco}.\n`);
+    console.log(`Questão 1 - Apartamento\n Apartamento com ${apartamento.quartos} quartos, localizado no ${apartamento.andar}º andar da ${apartamento.endereco}.\n`);
     
 //2
 let produtoEmbalado = {
@@ -15,7 +15,7 @@ let produtoEmbalado = {
     peso: 1.5,
     preco:3500.00
     };
-    console.log('Questão 2\n O produto embalado',produtoEmbalado.nome,'da categoria', produtoEmbalado.categoria, 'pesando', produtoEmbalado.peso,'kg, esta a venda por R$', produtoEmbalado.preco,".")
+    console.log('Questão 2 - Produto\n O produto embalado',produtoEmbalado.nome,'da categoria', produtoEmbalado.categoria, 'pesando', produtoEmbalado.peso,'kg, esta a venda por R$', produtoEmbalado.preco,".")
     
 //3
 class Imovel {
@@ -33,7 +33,7 @@ class Imovel {
     const casa = new Imovel(4, "Casa", "Rua da Amizade, 789 - Bairro Alegre");
     const apartamentos = new Imovel(2, "Apartamento", "Avenida da Paz, 123 - Centro");
     
-    console.log("\nQuetão 3\n",casa.exibirInformacoes());
+    console.log("\nQuestão 3 - Imovel\n",casa.exibirInformacoes());
     console.log(apartamentos.exibirInformacoes());
     
 //4
@@ -51,5 +51,5 @@ class veiculo {
     
     const carro = new veiculo("Toyota", "Corolla", 2022);
     const motocicleta = new veiculo("Honda","CBR 600RR", 2021 );
-    console.log("\nQuestão 4\n",carro.exibirDetalhes());
+    console.log("\nQuestão 4 - Veiculo\n",carro.exibirDetalhes());
     console.log(motocicleta.exibirDetalhes())
