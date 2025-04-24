b377fd29320ffa9743f9f21dcf869966a008ffc9
vamp-ctrl <victoraugustomauriciopereira@gmail.com>
2025-04-24 11:35:48 -0400

    fgsgf

--- Objetos/1.js
+++ Objetos/1.js
@@ -6,22 +6,22 @@ let apartamento = {
     andar: 7
     };
     
-    console.log(`Apartamento com ${apartamento.quartos} quartos, localizado no ${apartamento.andar}º andar da ${apartamento.endereco}.`);
+    console.log(`Quetão 1\n Apartamento com ${apartamento.quartos} quartos, localizado no ${apartamento.andar}º andar da ${apartamento.endereco}.\n`);
     
 //2
-let produtoEmbalado ={
+let produtoEmbalado = {
     nome: "Laptop HP",
     categoria:"eletronicos",
     peso: 1.5,
     preco:3500.00
     };
-    console.log('o produto embalado',produtoEmbalado.nome,'da categoria', produtoEmbalado.categoria, 'pesando', produtoEmbalado.peso,'kg, esta a venda por R$', produtoEmbalado.preco)
+    console.log('Questão 2\n O produto embalado',produtoEmbalado.nome,'da categoria', produtoEmbalado.categoria, 'pesando', produtoEmbalado.peso,'kg, esta a venda por R$', produtoEmbalado.preco,".")
     
 //3
 class Imovel {
-    constructor(quarto, tipo, enderecos) {
-    this.quarto = quarto;
-    this.tipos = tipo;
+    constructor(quarto, tipos, enderecos) {
+    this.quarto = quarto,
+    this.tipos = tipos,
     this.enderecos = enderecos;
     }
     
@@ -33,15 +33,15 @@ class Imovel {
     const casa = new Imovel(4, "Casa", "Rua da Amizade, 789 - Bairro Alegre");
     const apartamentos = new Imovel(2, "Apartamento", "Avenida da Paz, 123 - Centro");
     
-    console.log(casa.exibirInformacoes());
+    console.log("\nQuetão 3\n",casa.exibirInformacoes());
     console.log(apartamentos.exibirInformacoes());
     
 //4
 class veiculo {
     constructor(marca, modelo, ano) {
-    this.marca = marca;
-    this.modelo = modelo
-    this.ano = ano;
+    this.marca = marca,
+    this.modelo = modelo,
+    this.ano = ano
     }
     
     exibirDetalhes() {
@@ -51,5 +51,5 @@ class veiculo {
     
     const carro = new veiculo("Toyota", "Corolla", 2022);
     const motocicleta = new veiculo("Honda","CBR 600RR", 2021 );
-    console.log(carro.exibirDetalhes());
+    console.log("\nQuestão 4\n",carro.exibirDetalhes());
     console.log(motocicleta.exibirDetalhes())
